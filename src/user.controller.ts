6f95e3d79886f8e39a98f15084991e67fb6a9550
Ankit <private.ankit047@gmail.com>
2024-12-11 00:07:46 +0530

    reqest express

--- src/user.controller.ts
+++ src/user.controller.ts
@@ -1,18 +1,24 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get } from "@nestjs/common";
-
+import { Controller, Get,Req } from "@nestjs/common";
+import { of } from "rxjs";
+import {Request} from  'express'
 
 @Controller('/users')
 export class UsersController {
     @Get('/profile')
-    getProfile(){
+    getProfile(@Req() req: Request){
+        console.log(req)
         // request handler
-        return new Promise ((resolve,reject)=>{
-            resolve({
-                name:"Ankit",
-                class:"B-Tech CSE",
-                status:"online"
-            })
+        // return new Promise ((resolve,reject)=>{
+        //     resolve({
+        //         name:"Ankit",
+        //         class:"B-Tech CSE",
+        //         status:"online"
+        //     })
+        // })
+
+        return of({
+            hello:"World"
         })
     }
     
